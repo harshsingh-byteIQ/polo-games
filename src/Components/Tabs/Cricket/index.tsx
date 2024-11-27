7e164fa18bcd743c9b2ae5981e4b7c88b62e7868
harshsingh-byteIQ <harshsingh.byteiq@gmail.com>
2024-11-27 15:09:20 +0530

    Added API for cricket , soccer and tennis

--- src/Components/Tabs/Cricket/index.tsx
+++ src/Components/Tabs/Cricket/index.tsx
@@ -1,121 +1,137 @@
-import { Badge, Row, Col } from "antd";
+import { Badge, Row, Col, message, Spin } from "antd";
 import { PlayCircleOutlined, TagOutlined } from "@ant-design/icons";
-import styles from "../tabs.module.scss"
-const cricketData = [
-    {
-      key: "1",
-      match: "India v Australia",
-      league: "International Cricket",
-      status: "live",
-      date: "24 Nov",
-      time: "7:00 PM",
-      odds: [
-        { key: "1", value: "1.75", extra: "3.4", color: "#add8e6" },
-        { key: "X", value: "2.05", extra: "1.5", color: "#ffc0cb" },
-        { key: "2", value: "2.45", extra: "3.4", color: "#add8e6" },
-      ],
-    },
-    {
-      key: "2",
-      match: "England v South Africa",
-      league: "ODI Championship",
-      status: "upcoming",
-      date: "26 Nov",
-      time: "3:30 PM",
-      odds: [
-        { key: "1", value: "1.85", extra: "2.3", color: "#add8e6" },
-        { key: "X", value: "2.25", extra: "4.5", color: "#ffc0cb" },
-        { key: "2", value: "2.75", extra: "1.3", color: "#add8e6" },
-      ],
-    },
-  ];
-  
+import styles from "../tabs.module.scss";
+import axios from "axios";
+import { useEffect, useState } from "react";
 
 const CricketSection = () => {
-  return (
-    <div style={{ margin: "16px" }}>
-      <div
-        className={styles.tableWrapper}
-      >
-        <PlayCircleOutlined style={{ fontSize: "24px", marginRight: "16px" }} />
-        <h3 style={{ margin: 0 }}>Cricket</h3>
-      </div>
+  const [datasource, setDataSource] = useState<any>([]);
+  const [loading, setLoading] = useState<boolean>(false);
 
-      <div
-        className={styles.table}
-      >
-        {cricketData.map((item) => (
-          <div
-            key={item.key}
-            className={styles.tableHeader}
-          >
-            <div
-              style={{
-                flex: 2,
-                marginRight: "16px",
-                display: "flex",
-                justifyContent: "space-between",
-                alignItems:"center"
-              }}
-            >
-              <div>
-                <div style={{ fontWeight: "bold" }}>{item.match}</div>
-                <div style={{ color: "#888", fontSize: "12px" }}>
-                  {item.league}
+  const ModifyData = (data: []) => {
+    const ModifiedData = data?.map((ele: any) => {
+      const tempSplit = ele?.eventName?.split("/");
+      console.log(tempSplit);
+      const obj = {
+        match: tempSplit[0],
+        status: ele?.inPlay === "True" ? "live" : "upcoming",
+        time: tempSplit[1],
+        odds: [
+          { key: "1", value: ele?.back1, extra: ele?.back11, color: "#add8e6" },
+          { key: "2", value: ele?.back12, extra: ele?.lay1, color: "#ffc0cb" },
+          { key: "3", value: ele?.lay11, extra: ele?.lay12, color: "#add8e6" },
+        ],
+      };
+      return obj;
+    });
+
+    setDataSource(ModifiedData);
+  };
+
+  const getApiData = async () => {
+    setLoading(true);
+
+    try {
+      const response = await axios.get(
+        "https://marketsarket.qnsports.live/getcricketmatches"
+      );
+      const data = response.data;
+      ModifyData(data);
+    } catch (err) {
+      console.error(err);
+      message.error(
+        "Something went wrong in fetching the data.. Please wait while we fix the issue"
+      );
+    } finally {
+      setLoading(false);
+    }
+  };
+
+  useEffect(() => {
+    getApiData();
+  }, []);
+
+  return (
+    <Spin spinning={loading}>
+      <div style={{ margin: "16px" }}>
+        <div className={styles.tableWrapper}>
+          <PlayCircleOutlined
+            style={{ fontSize: "24px", marginRight: "16px" }}
+          />
+          <h3 style={{ margin: 0 }}>Cricket</h3>
+        </div>
+
+        <div className={styles.table}>
+          {datasource.map((item: any) => (
+            <div key={item.key} className={styles.tableHeader}>
+              <div
+                style={{
+                  flex: 2,
+                  marginRight: "16px",
+                  display: "flex",
+                  justifyContent: "space-between",
+                  alignItems: "center",
+                }}
+              >
+                <div>
+                  <div style={{ fontWeight: "bold" }}>{item.match}</div>
+                  <div style={{ color: "#888", fontSize: "12px" }}>
+                    {item.league}
+                  </div>
+                </div>
+                <div>
+                  {item.status === "live" && (
+                    <Badge status="success" style={{ marginRight: 8 }} />
+                  )}
+                  {item.time}
                 </div>
               </div>
-              <div>
-                {item.status === "live" && (
-                  <Badge status="success" style={{ marginRight: 8 }} />
-                )}
-                {item.date} - {item.time}
+
+              <div className={styles.tagIcon}>
+                <TagOutlined style={{ fontSize: "24px" }} />
               </div>
-            </div>
 
-            <div className={styles.tagIcon}>
-              <TagOutlined style={{ fontSize: "24px" }} />
+              <Row gutter={8} style={{ flex: 3 }}>
+                {item.odds.map((odd: any) => (
+                  <>
+                    <Col
+                      span={4}
+                      key={odd.key}
+                      style={{
+                        background: odd.color,
+                        padding: "8px",
+                        border: "1px solid white",
+                        textAlign: "center",
+                      }}
+                    >
+                      <div style={{ fontWeight: "bold" }}>{odd.value}</div>
+                      <div style={{ fontSize: "10px", color: "#666" }}>
+                        {odd.extra}
+                      </div>
+                    </Col>
+                    <Col
+                      span={4}
+                      key={odd.key}
+                      style={{
+                        background: odd.color,
+                        padding: "8px",
+                        border: "1px solid white",
+                        textAlign: "center",
+                      }}
+                    >
+                      <div style={{ fontWeight: "bold" }}>{odd.value}</div>
+                      <div style={{ fontSize: "10px", color: "#666" }}>
+                        {odd.extra}
+                      </div>
+                    </Col>
+                  </>
+                ))}
+              </Row>
             </div>
-
-            <Row gutter={8} style={{ flex: 3 }}>
-              {item.odds.map((odd) => (
-                <>
-                  <Col
-                    span={4}
-                    key={odd.key}
-                    style={{
-                      background: odd.color,
-                      padding: "8px",
-                      border: "1px solid white",
-                      textAlign: "center",
-                    }}
-                  >
-                    <div style={{ fontWeight: "bold" }}>{odd.value}</div>
-                    <div style={{ fontSize: "10px", color: "#666" }}>
-                      {odd.extra}
-                    </div>
-                  </Col>
-                  <Col
-                    span={4}
-                    key={odd.key}
-                    style={{
-                      background: odd.color,
-                      padding: "8px",
-                      border: "1px solid white",
-                      textAlign: "center",
-                    }}
-                  >
-                    <div style={{ fontWeight: "bold" }}>{odd.value}</div>
-                    <div style={{ fontSize: "10px", color: "#666" }}>
-                      {odd.extra}
-                    </div>
-                  </Col>
-                </>
-              ))}
-            </Row>
-          </div>
-        ))}
+          ))}
+        </div>
       </div>
-    </div>
+    </Spin>
   );
 };
 
--- src/Components/Tabs/Soccer/index.tsx
+++ src/Components/Tabs/Soccer/index.tsx
@@ -1,146 +1,139 @@
-import { Badge, Row, Col } from "antd";
+import { Badge, Row, Col, message, Spin } from "antd";
 import { PlayCircleOutlined, TagOutlined } from "@ant-design/icons";
-import styles from "../tabs.module.scss"
-const dataSource = [
-    {
-      key: "1",
-      match: "Samsunspor v Alanyaspor",
-      league: "Turkish Soccer",
-      status: "live",
-      date: "24 Nov",
-      time: "6:30 PM",
-      odds: [
-        { key: "1", value: "3.65", extra: "446.33", color: "#add8e6" },
-        { key: "X", value: "3.75", extra: "45.56", color: "#ffc0cb" },
-        { key: "2", value: "1.8", extra: "529.98", color: "#add8e6" },
-      ],
-    },
-    {
-      key: "2",
-      match: "Samsunspor v Alanyaspor",
-      league: "Turkish Soccer",
-      status: "live",
-      date: "24 Nov",
-      time: "6:30 PM",
-      odds: [
-        { key: "3", value: "3.65", extra: "446.33", color: "#add8e6" },
-        { key: "X", value: "3.75", extra: "45.56", color: "#ffc0cb" },
-        { key: "2", value: "1.8", extra: "529.98", color: "#add8e6" },
-      ],
-    },
-    {
-      key: "3",
-      match: "Samsunspor v Alanyaspor",
-      league: "Turkish Soccer",
-      status: "live",
-      date: "24 Nov",
-      time: "6:30 PM",
-      odds: [
-        { key: "1", value: "3.65", extra: "446.33", color: "#add8e6" },
-        { key: "X", value: "3.75", extra: "45.56", color: "#ffc0cb" },
-        { key: "2", value: "1.8", extra: "529.98", color: "#add8e6" },
-      ],
-    },
-    {
-      key: "4",
-      match: "Samsunspor v Alanyaspor",
-      league: "Turkish Soccer",
-      status: "live",
-      date: "24 Nov",
-      time: "6:30 PM",
-      odds: [
-        { key: "1", value: "3.65", extra: "446.33", color: "#add8e6" },
-        { key: "X", value: "3.75", extra: "45.56", color: "#ffc0cb" },
-        { key: "2", value: "1.8", extra: "529.98", color: "#add8e6" },
-      ],
-    },
-  ];
-  
+import styles from "../tabs.module.scss";
+import { useEffect, useState } from "react";
+import axios from "axios";
+
+
 
 const SoccerSection = () => {
-  return (
-    <div style={{ margin: "16px" }}>
-      <div
-         className={styles.tableWrapper}
-      >
-        <PlayCircleOutlined style={{ fontSize: "24px", marginRight: "16px" }} />
-        <h3 style={{ margin: 0 }}>Soccer</h3>
-      </div>
 
-      <div
-         className={styles.table}
-      >
-        {dataSource.map((item) => (
-          <div
-            key={item.key}
-            className={styles.tableHeader}
-          >
-            <div
-              style={{
-                flex: 2,
-                marginRight: "16px",
-                display: "flex",
-                justifyContent: "space-between",
-              }}
-            >
-              <div>
-                <div style={{ fontWeight: "bold" }}>{item.match}</div>
-                <div style={{ color: "#888", fontSize: "12px" }}>
-                  {item.league}
+  const [datasource, setDataSource] = useState<any>([]);
+const [loading, setLoading] = useState<boolean>(false);
+
+const ModifyData = (data: []) => {
+  const ModifiedData = data?.map((ele: any) => {
+    const obj = {
+      match: ele?.ename,
+      status: ele?.iplay? "live" : "upcoming",
+      league: ele?.cname,
+      time: ele?.stime,
+      odds: [
+        { key: "1", value: ele?.section[0]?.odds[0]?.odds, extra: ele?.section[0]?.odds[1]?.odds, color: "#add8e6" },
+        { key: "2", value: ele?.section[1]?.odds[0]?.odds, extra: ele?.section[1]?.odds[1]?.odds, color: "#ffc0cb" },
+        { key: "3", value: ele?.section[2]?.odds[0]?.odds, extra: ele?.section[2]?.odds[1]?.odds, color: "#add8e6" },
+      ],
+    };
+    console.log(obj);
+    return obj;
+  });
+
+  setDataSource(ModifiedData);
+};
+
+const getApiData = async () => {
+  setLoading(true);
+
+  try {
+    const response = await axios.get(
+      "https://marketsarket.qnsports.live/getsoccermatches"
+    );
+    const data = response.data;
+    ModifyData(data);
+  } catch (err) {
+    console.error(err);
+    message.error(
+      "Something went wrong in fetching the data.. Please wait while we fix the issue"
+    );
+  } finally {
+    setLoading(false);
+  }
+};
+
+useEffect(() => {
+  getApiData();
+}, []);
+
+  return (
+    <Spin spinning={loading}>
+      <div style={{ margin: "16px" }}>
+        <div className={styles.tableWrapper}>
+          <PlayCircleOutlined
+            style={{ fontSize: "24px", marginRight: "16px" }}
+          />
+          <h3 style={{ margin: 0 }}>Soccer</h3>
+        </div>
+
+        <div className={styles.table}>
+          {datasource.map((item : any) => (
+            <div key={item.key} className={styles.tableHeader}>
+              <div
+                style={{
+                  flex: 2,
+                  marginRight: "16px",
+                  display: "flex",
+                  justifyContent: "space-between",
+                }}
+              >
+                <div>
+                  <div style={{ fontWeight: "bold" }}>{item.match}</div>
+                  <div style={{ color: "#888", fontSize: "12px" }}>
+                    {item.league}
+                  </div>
+                </div>
+                <div>
+                  {item.status === "live" && (
+                    <Badge status="success" style={{ marginRight: 8 , whiteSpace: "nowrap" }} />
+                  )}
+                  {item.time}
                 </div>
               </div>
-              <div>
-                {item.status === "live" && (
-                  <Badge status="success" style={{ marginRight: 8 }} />
-                )}
-                {item.date} - {item.time}
+
+              <div className={styles.tagIcon}>
+                <TagOutlined style={{ fontSize: "24px" }} />
               </div>
-            </div>
 
-            <div className={styles.tagIcon}>
-              <TagOutlined style={{ fontSize: "24px" }} />
+              <Row gutter={8} style={{ flex: 3 }}>
+                {item.odds.map((odd : any) => (
+                  <>
+                    <Col
+                      span={4}
+                      key={odd.key}
+                      style={{
+                        background: odd.color,
+                        padding: "8px",
+                        border: "1px solid white",
+                        textAlign: "center",
+                      }}
+                    >
+                      <div style={{ fontWeight: "bold" }}>{odd.value}</div>
+                      <div style={{ fontSize: "10px", color: "#666" }}>
+                        {odd.extra}
+                      </div>
+                    </Col>
+                    <Col
+                      span={4}
+                      key={odd.key}
+                      style={{
+                        background: odd.color,
+                        padding: "8px",
+                        border: "1px solid white",
+                        textAlign: "center",
+                      }}
+                    >
+                      <div style={{ fontWeight: "bold" }}>{odd.value}</div>
+                      <div style={{ fontSize: "10px", color: "#666" }}>
+                        {odd.extra}
+                      </div>
+                    </Col>
+                  </>
+                ))}
+              </Row>
             </div>
-
-            <Row gutter={8} style={{ flex: 3 }}>
-              {item.odds.map((odd) => (
-                <>
-                  <Col
-                    span={4}
-                    key={odd.key}
-                    style={{
-                      background: odd.color,
-                      padding: "8px",
-                      border: "1px solid white",
-                      textAlign: "center",
-                    }}
-                  >
-                    <div style={{ fontWeight: "bold" }}>{odd.value}</div>
-                    <div style={{ fontSize: "10px", color: "#666" }}>
-                      {odd.extra}
-                    </div>
-                  </Col>
-                  <Col
-                    span={4}
-                    key={odd.key}
-                    style={{
-                      background: odd.color,
-                      padding: "8px",
-                      border: "1px solid white",
-                      textAlign: "center",
-                    }}
-                  >
-                    <div style={{ fontWeight: "bold" }}>{odd.value}</div>
-                    <div style={{ fontSize: "10px", color: "#666" }}>
-                      {odd.extra}
-                    </div>
-                  </Col>
-                </>
-              ))}
-            </Row>
-          </div>
-        ))}
+          ))}
+        </div>
       </div>
-    </div>
+    </Spin>
   );
 };
 
