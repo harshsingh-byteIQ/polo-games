10ba0f1e2d8703cea5923de2b8118c9d09017b79
harshsingh-byteIQ <harshsingh.byteiq@gmail.com>
2024-11-27 16:39:52 +0530

    removed the preseleted option in the side menu

--- src/Components/SideBar/index.tsx
+++ src/Components/SideBar/index.tsx
@@ -1,77 +1,77 @@
-import { Layout, Menu } from 'antd';
+import { Layout, Menu } from "antd";
 import {
   HomeOutlined,
   PlayCircleOutlined,
   TrophyOutlined,
   RadarChartOutlined,
-} from '@ant-design/icons';
-import styles from './sidebar.module.scss';
+} from "@ant-design/icons";
+import styles from "./sidebar.module.scss";
 
 const { Sider } = Layout;
 
 const SideBar = () => {
-  const menuItems : any = [
+  const menuItems: any = [
     {
-      key: 'home',
+      key: "home",
       icon: <HomeOutlined />,
-      label: 'Home',
+      label: "Home",
     },
     {
-      key: 'in-play',
+      key: "in-play",
       icon: <PlayCircleOutlined />,
-      label: 'In Play',
+      label: "In Play",
     },
     {
-      key: 'separator', 
-      type: 'group', 
+      key: "separator",
+      type: "group",
       label: (
         <div
           style={{
-            height: '1px',
-            background: '#dcdcdc',
-            margin: '1px 0',
+            height: "1px",
+            background: "#dcdcdc",
+            margin: "1px 0",
           }}
         />
       ),
     },
     {
-      key: 'cricket',
+      key: "cricket",
       icon: <TrophyOutlined />,
-      label: 'Cricket',
+      label: "Cricket",
       children: [
-        { key: 'cricket-odi', label: 'One Day Internationals' },
-        { key: 'cricket-t20', label: 'Women’s International Twenty20 Matches' },
-        { key: 'cricket-t10', label: 'Abu Dhabi T10' },
-        { key: 'cricket-league', label: 'Global Super League T20' },
+        { key: "cricket-odi", label: "One Day Internationals" },
+        { key: "cricket-t20", label: "Women’s International Twenty20 Matches" },
+        { key: "cricket-t10", label: "Abu Dhabi T10" },
+        { key: "cricket-league", label: "Global Super League T20" },
       ],
     },
     {
-      key: 'soccer',
+      key: "soccer",
       icon: <RadarChartOutlined />,
-      label: 'Soccer',
+      label: "Soccer",
       children: [
-        { key: 'soccer-premier', label: 'Premier League' },
-        { key: 'soccer-champions', label: 'Champions League' },
-        { key: 'soccer-worldcup', label: 'World Cup' },
+        { key: "soccer-premier", label: "Premier League" },
+        { key: "soccer-champions", label: "Champions League" },
+        { key: "soccer-worldcup", label: "World Cup" },
       ],
     },
     {
-      key: 'tennis',
+      key: "tennis",
       icon: <RadarChartOutlined />,
-      label: 'Tennis',
+      label: "Tennis",
       children: [
-        { key: 'tennis-grand-slam', label: 'Grand Slam' },
-        { key: 'tennis-atp', label: 'ATP Tours' },
-        { key: 'tennis-wta', label: 'WTA Tours' },
+        { key: "tennis-grand-slam", label: "Grand Slam" },
+        { key: "tennis-atp", label: "ATP Tours" },
+        { key: "tennis-wta", label: "WTA Tours" },
       ],
     },
     {
-      key: 'horse-racing',
+      key: "horse-racing",
       icon: <TrophyOutlined />,
-      label: 'Horse Racing',
+      label: "Horse Racing",
       children: [
-        { key: 'horse-racing-local', label: 'Local Races' },
-        { key: 'horse-racing-international', label: 'International Races' },
+        { key: "horse-racing-local", label: "Local Races" },
+        { key: "horse-racing-international", label: "International Races" },
       ],
     },
   ];
@@ -79,14 +79,14 @@ const SideBar = () => {
   return (
     <Sider
       style={{
-        background: '#fff',
-        borderRight: '1px solid #dcdcdc',
+        background: "#fff",
+        borderRight: "1px solid #dcdcdc",
       }}
     >
       <Menu
         mode="inline"
-        defaultSelectedKeys={['home']}
-        defaultOpenKeys={['home', 'cricket']}
+        defaultSelectedKeys={["home"]}
+        defaultOpenKeys={[]} // No open menus by default
         items={menuItems}
         className={styles.sidebarMenu}
       />
